47f473fd1babf1d26c4eee0ad62e9b0217cd8f08
Pranav Jothimani <pranav.2001146@srec.ac.in>
2023-08-08 13:58:30 +0530

    fix: made changes to commands

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', 'C-p', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pf', '<cmd>Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, {})
+ -- vim.keymap.set('n', '<C-g>','<cmd>Telescope git_files<CR>')
+vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>')
+vim.keymap.set('n', '<leader>gi', '<cmd>Telescope lsp_implementations<CR>')
 
 require('telescope').load_extension('media_files')
 
